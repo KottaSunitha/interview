54010b225755a0926a82e51398b7045c34f6f2e0
KottaSunitha <kottasunitha1997@gmail.com>
2024-03-08 15:30:38 +0000

    newfile13

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 905417996861.dkr.ecr.us-east-2.amazonaws.com
 docker build -t test-ecr .
-TIME=$( date '+%F_%H:%M:%S' )
+TIME=$( date '+%F_%H_%M_%S' )
 docker tag test-ecr:latest 905417996861.dkr.ecr.us-east-2.amazonaws.com/test-ecr:$TIME
 docker push 905417996861.dkr.ecr.us-east-2.amazonaws.com/test-ecr:$TIME
 docker run -itd -p 8000:80 905417996861.dkr.ecr.us-east-2.amazonaws.com/test-ecr:$TIME
